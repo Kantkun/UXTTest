b76dbacb58badbc57948df9106476dabd47e4fd7
Nattanon Tesavibul <nattanon.t@g-able.com>
2022-07-14 16:10:24 +0700

    add endpoint

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div>
       <div className="m-2 text-success fw-bold fs-1">UXT : Beer Test</div>
+      <div className="m-2">Endpoint : https://api.sampleapis.com/beers/ale</div>
     </div>
   );
 }
